9179cf8b18bab5f93cc9d1a5b4340594b3cc6733
PablosGon <100768678+PablosGon@users.noreply.github.com>
2024-04-08 23:29:12 +0200

    added Pasajero entity

---
+++ webapi/webapi/Models/Pasajero.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace webapi.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Pasajero
+    {
+
+        [BsonElement("nombre")]
+        public string Nombre { get; set; }
+
+        [BsonElement("correo")]
+        public string Correo { get; set; }
+
+        [BsonElement("telefono")]
+        public string Telefono { get; set; }
+
+        [BsonElement("ubicacionRecogida")]
+        public Ubicacion UbicacionRecogida { get; set; }
+
+        [BsonElement("comentarios")]
+        public string Comentarios { get; set; }
+
+    }
+}
