1a458b6a5a553de2a76ec80b374f7031c2234c94
PablosGon <100768678+PablosGon@users.noreply.github.com>
2024-06-13 11:50:02 +0200

    fixed prod api base url

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    BASE_API_URL: "https://carpooling-web-app.azure-api.net/",
+    BASE_API_URL: "https://carpooling-web-app.azure-api.net/api/",
     BASE_CLOUDINARY_IMAGE_URL: "https://res.cloudinary.com/dilfzvvw4/image/upload/v1716716882/"
 };
