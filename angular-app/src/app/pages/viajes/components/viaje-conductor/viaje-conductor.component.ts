33575b4d5a62077f02ec23a91a034b245880112e
PablosGon <100768678+PablosGon@users.noreply.github.com>
2024-07-10 11:49:13 +0200

    added comentarios to viaje-conductor

--- angular-app/src/app/pages/viajes/components/viaje-conductor/viaje-conductor.component.ts
+++ angular-app/src/app/pages/viajes/components/viaje-conductor/viaje-conductor.component.ts
@@ -14,6 +14,7 @@ import { environment } from '../../../../../environments/environment';
 export class ViajeConductorComponent {
 
   @Input() idUsuario? : number
+  @Input() comentarios? : string
 
   readonly imagesDefault = ImagesDefault
 
